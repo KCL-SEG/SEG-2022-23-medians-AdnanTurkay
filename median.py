e279d5c119e38652220f2c9c2eb859b84d968f6f
AdnanTurkay <adntrky@gmail.com>
2022-11-21 11:10:30 +0000

    Initial solution for "Medians"

--- median.py
+++ median.py
@@ -1,6 +1,23 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def median(n):
+    if len(n) == 0:
+        return 0
+    if len(n) == 1:
+        return n[0]
+
+    n.sort()
+    length = len(n)
+
+    if length % 2 == 1:
+        return n[int((length-1)/2)]
+    elif length % 2 == 0:
+        result = (n[int(length/2)] + n[int((length/2)-1)])/2
+        return result
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +26,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median(numbers))
